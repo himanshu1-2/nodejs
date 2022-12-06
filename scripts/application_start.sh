43fc0505ed335d4aff63186a2cab82d6ba13ea3d
himanshu1-2 <34888386+himanshu1-2@users.noreply.github.com>
2022-12-06 22:00:59 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-aws s3 cp s3://generico-node-internal/secrets-keep-private/generico-crm-node/pre-prod/.env .
+#aws s3 cp s3://generico-node-internal/secrets-keep-private/generico-crm-node/pre-prod/.env .
 
 npm install
 sudo chmod -R 777 public/
