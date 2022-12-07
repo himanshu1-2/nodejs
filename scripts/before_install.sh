f45de5f57ec3ae8617a91aee4b3880b23c62b3de
himanshu1-2 <34888386+himanshu1-2@users.noreply.github.com>
2022-12-07 14:29:35 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install  16.0.0
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install  16.0.0
 
 # aws s3 cp 's3://generico-node-internal/secrets-keep-private/generico-crm-node/staging/staging.env' 'tmp/'
 
