99b640c2e08c47a70240fb8f684f3e4f9a5d177e
Himanshuyyh <himanshu@atraphygital.com>
2022-12-08 21:20:19 +0530

    before install echo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.0.0
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 16.0.0
 
 # aws s3 cp 's3://generico-node-internal/secrets-keep-private/generico-crm-node/staging/staging.env' 'tmp/'
 
 
-DIR="/home/nodejs"
-if [ -d "$DIR" ]; then
+#DIR="/home/nodejs"
+#if [ -d "$DIR" ]; then
   echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+#else
+ # echo "Creating ${DIR} directory"
+  #mkdir ${DIR}
+#fi
