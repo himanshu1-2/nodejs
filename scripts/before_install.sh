b6ff89aff31d2c75f1b799e5410e554e8521f22f
Himanshuyyh <himanshu@atraphygital.com>
2022-12-08 21:09:41 +0530

    before

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ nvm install 16.0.0
 # aws s3 cp 's3://generico-node-internal/secrets-keep-private/generico-crm-node/staging/staging.env' 'tmp/'
 
 
-DIR="/home/ubuntu/node-projects/generico-crm-node"
+DIR="/home/nodejs"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
